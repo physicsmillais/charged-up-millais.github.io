0dc2d6ce00a4f1ae84ef4546bb665eb270e95568
physicsmillais <effie@effie.bio>
2024-12-15 23:55:56 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
 const PDFPage = () => {
   return (
     <div style={{ height: "100vh", overflowY: "auto" }}>
-      <object
-        data="/ChargedUpDec2024.pdf"
-        type="application/pdf"
-        width="100%"
-        height="100%"
-      >
-        <p>
-          Your browser does not support displaying PDFs. You can{" "}
-          <a href="/ChargedUpDec2024.pdf" download>
-            download the PDF
-          </a>{" "}
-          instead.
-        </p>
-      </object>
+      <object data="ChargedUpDec2024.pdf" type="application/pdf">
+        <embed src="ChargedUpDec2024.pdf" type="application/pdf" />
+    </object>
     </div>
   );
 };
