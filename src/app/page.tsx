3ae25c10666fbe6948d5c4060383b7a6af981dad
physicsmillais <effie@effie.bio>
2024-12-16 00:00:21 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 const PDFPage = () => {
   return (
-    <div className="w-full h-full">
-      <object data="ChargedUpDec2024.pdf" type="application/pdf">
-        <embed src="ChargedUpDec2024.pdf" type="application/pdf" />
-    </object>
+    <div className="w-full h-screen">
+      <object 
+        data="ChargedUpDec2024.pdf" 
+        type="application/pdf" 
+        className="w-full h-full"
+      >
+        <embed 
+          src="ChargedUpDec2024.pdf" 
+          type="application/pdf" 
+          className="w-full h-full"
+        />
+      </object>
     </div>
   );
 };
 
 export default PDFPage;
-
-
