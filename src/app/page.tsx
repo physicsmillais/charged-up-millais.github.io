a1f9530a53d07e88b9d6dc2e6341fd2eddd09257
physicsmillais <effie@effie.bio>
2024-12-16 00:15:37 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-
 const PDFPage = () => {
   return (
-    <div className="w-full h-screen overflow-auto">
-      {/* PDF.js worker file */}
-      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.8.162/build/pdf.worker.min.js`}>
-        <div className="w-full h-full">
-          <Viewer fileUrl="/ChargedUpDec2024.pdf" />
-        </div>
-      </Worker>
+    <div className="w-full h-screen">
+      <object 
+        data="ChargedUpDec2024.pdf" 
+        type="application/pdf" 
+        className="w-full h-full"
+      >
+        <embed 
+          src="ChargedUpDec2024.pdf" 
+          type="application/pdf" 
+          className="w-full h-full"
+        />
+      </object>
     </div>
   );
 };
