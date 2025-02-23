f024b4e835e5be800a75aa962ed4b4909472762e
physicsmillais <effie@effie.bio>
2025-02-23 21:48:44 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ const PDFPage = () => {
   return (
     <div className="w-screen h-screen overflow-auto flex justify-center items-center">
       <iframe
-        src="https://drive.google.com/file/d/1an0t8iQCeqk4lvYmAZU-bvrxD3MtZ6DB/preview?usp=sharing?embedded=true"
+        src="https://drive.google.com/file/d/1Yu0ebFevARHybUE96I97a-7hsYwUKluG/preview?usp=sharing?embedded=true"
         style={{
-          width: '100%',
-          height: '100%',
-          border: 'none',
+          width: "100%",
+          height: "100%",
+          border: "none",
         }}
         frameBorder={0}
         title="PDF Viewer"
