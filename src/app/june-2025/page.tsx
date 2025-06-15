8205dcee6bbb2fbf005ea7ca89d1f5f681ab6883
physicsmillais <effie@effie.bio>
2025-06-15 23:21:28 +0100

    Create page.tsx

---
+++ src/app/june-2025/page.tsx
@@ -0,0 +1,20 @@
+const PDFPage = () => {
+  return (
+    <div className="w-screen h-screen overflow-auto flex justify-center items-center">
+      <iframe
+        src="https://drive.google.com/file/d/1an0t8iQCeqk4lvYmAZU-bvrxD3MtZ6DB/preview?usp=sharing?embedded=true"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        frameBorder={0}
+        title="PDF Viewer"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+};
+
+export default PDFPage;
