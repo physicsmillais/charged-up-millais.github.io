7972a55fc22b3790f5ec67333889feac48287641
physicsmillais <effie@effie.bio>
2025-10-09 08:47:34 +0100

    added october 2025 pdf link

--- src/app/october-2025/page.tsx
+++ src/app/october-2025/page.tsx
@@ -1,5 +1,23 @@
+const PDFPage = () => {
+  return (
+    <div className="w-screen h-screen overflow-auto flex justify-center items-center">
+      <iframe
+        src="https://drive.google.com/file/d/10bbyHtvrMSu8oiWfD1i05wczRHAVKpxj/preview?usp=sharing?embedded=true"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        frameBorder={0}
+        title="PDF Viewer"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+};
+
+export default PDFPage;
 
 
 
-
-https://drive.google.com/file/d/10bbyHtvrMSu8oiWfD1i05wczRHAVKpxj/view?usp=sharing
