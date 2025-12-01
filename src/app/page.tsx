2acc96501791ac19a43c6b75d54e9f4a5e2b712a
physicsmillais <effie@effie.bio>
2025-12-01 19:56:07 +0000

    Updated dec 25 link homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ const PDFPage = () => {
   return (
     <div className="w-screen h-screen overflow-auto flex justify-center items-center">
       <iframe
-        src="https://drive.google.com/file/d/1b4coVUen0AUh_HA7d0SMnis1k845qn0Z/view?usp=sharing?embedded=true"
+        src="https://drive.google.com/file/d/1b4coVUen0AUh_HA7d0SMnis1k845qn0Z/preview?usp=sharing?embedded=true"
         style={{
           width: "100%",
           height: "100%",
