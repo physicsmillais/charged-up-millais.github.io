49cc2a53ce506cc4fb694ed0033e5fb6acea9b34
physicsmillais <effie@effie.bio>
2026-01-25 20:59:26 +0000

    added jan 26 issue

---
+++ src/app/january-2026/page.tsx
@@ -0,0 +1,20 @@
+const PDFPage = () => {
+  return (
+    <div className="w-screen h-screen overflow-auto flex justify-center items-center">
+      <iframe
+        src="https://drive.google.com/file/d/1Fe2VRtxVwY60TYUSVaoBl3ZgugCATSs1/preview?usp=sharing?embedded=true"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        frameBorder={0}
+        title="PDF Viewer"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+};
+
+export default PDFPage;
