4f657aa161ef4d6ccf94e633d6edb6cd8142e348
physicsmillais <effie@effie.bio>
2026-03-11 22:08:14 +0000

    Update page.tsx
    added Gdrive link

--- src/app/march-2026/page.tsx
+++ src/app/march-2026/page.tsx
@@ -1,1 +1,22 @@
+const PDFPage = () => {
+  return (
+    <div className="w-screen h-screen overflow-auto flex justify-center items-center">
+      <iframe
+        src="https://drive.google.com/file/d/1GJyctuV_CFhODLrQML3Jx9Shnz5Wibkl/preview?usp=sharing?embedded=true"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        frameBorder={0}
+        title="PDF Viewer"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+};
+
+export default PDFPage;
+
 
